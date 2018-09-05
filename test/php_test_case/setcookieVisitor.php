2f8dea914238b3247e903250d624883d49ea04d3
BoKyoungHan <21500777@handong.edu>
2018-09-05 20:46:24 +0900

    add test code

---
+++ test/php_test_case/setcookieVisitor.php
@@ -0,0 +1,9 @@
+<?php
+
+$value = 'something from somewhere';
+
+setcookie("TestCookie", $value);
+setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
+setcookie("TestCookie", $value, time()+3600, "/~rasmus/", "example.com", 1);
+
+?>
