7bffb8f2306be22029de3aab80cc93044d7e5757
BoKyoungHan <21500777@handong.edu>
2018-09-05 20:20:46 +0900

    delete some unnecesary code

--- test/php_test_case/XSS.php
+++ test/php_test_case/XSS.php
@@ -6,9 +6,10 @@
     $nonTaint1 = 13;
     $nonTaint2 = 14;
 
-    $taint3 = $taint1;
-    $taint4 = $taint2;
-
+    if(true){
+    	$taint3 = $taint1;
+    	$taint4 = $taint2;
+    }
 
     echo $nonTaint2;
     echo $taint3;
