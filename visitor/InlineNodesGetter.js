299a410d9db127fb8d4511577f2aac562835faf7
BoKyoungHan <21500777@handong.edu>
2018-10-05 15:53:21 +0900

    add inline node getter

---
+++ visitor/InlineNodesGetter.js
@@ -0,0 +1,22 @@
+const Visitor = require('./visitor');
+const _ = require('underscore');
+
+class InlineNodesGetter extends Visitor {
+    constructor() {
+        super();
+        this.nodes = [];
+    }
+
+    visit(node){
+        _.each( _.values(node), (value)=>{
+            if( _.has( value, 'kind')){
+                if(value.kind == 'inline')this.nodes.push(value);              
+            }
+            if(_.keys(value).length){
+                this.visit(value);
+            }
+        });
+    }
+}
+
+module.exports = InlineNodesGetter;
