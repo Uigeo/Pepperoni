f45f45c64065ce4a6814efc78eba6bf2efdc3b00
BoKyoungHan <21500777@handong.edu>
2018-11-27 00:10:15 +0900

    add bugReport()

--- detector/SetcookieDetector.js
+++ detector/SetcookieDetector.js
@@ -5,18 +5,45 @@ const _ = require('underscore');
 class SetcookieDetector extends Detector {
     constructor(){
       super();
+      this.cookies;
+      this.setcookieFinder;
+      this.path = path;
+
     }
 
     detect(node){
-        var setcookieFinder = new SetcookieFinder();
+        this.setcookieFinder = new SetcookieFinder();
 
-        node.accept(setcookieFinder);
+        node.accept(this.setcookieFinder);
         
-        setcookieFinder.execute();
+        this.setcookieFinder.execute();
 
+        this.bugReport();
+        console.log(this.cookies);
 
-        //TODO generate bug instance
-        //generateBug();
+    }
+
+    //두 가지 종류의 bug가 있어서 리포트를 다르게 해주어도 될 거 같다.
+    bugReport(){
+        this.cookies = this.setcookieFinder.danger_cookie.concat(this.setcookieFinder.weak_cookie);
+        return _.map(this.cookies, (vul)=>{
+            return {
+                'info' : {
+                    "category" : "Inadequate Parameter",
+                    "name" : "Dangerous setcookie Function",
+                    'ID' : 13,
+                    'rank' : 1,
+                    'CWE' : 1004
+                },
+                'SourceLine' : {
+                    "filename" : this.path,
+                    "start" :  vul.loc.start,
+                    "end" : vul.loc.end,
+                },
+                'message' : "The setcookie function's parameter was improperly entered.",
+                'sugestion' : "fix it"
+            }
+        } );
     }
 
 }
